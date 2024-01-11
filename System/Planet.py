bcfe196fe43c1bab2c7134eee093cac9dca1d07d
Randall Czajak <108299837+rcdczajak@users.noreply.github.com>
2024-01-11 01:47:57 +0000

    fourth commit

--- System/Planet.py
+++ System/Planet.py
@@ -2,11 +2,16 @@ import math
 
 class Planet:
 
-    def __init__ (self, iname, irad, im, idist):
+    def __init__ (self, iname, irad, im, idist, imoons, moonList):
         self.name = iname
         self.radius = irad
         self.mass = im
         self.distance = idist
+        self.numMoons = imoons
+        self.moonList = moonList
+
+    def __string__ (self):
+        return self.name
 
     def getName (self):
         return self.name
@@ -20,6 +25,12 @@ class Planet:
     def getDistance (self):
         return self.distance
     
+    def getMoons (self):
+        return self.numMoons
+    
+    def getMoonList (self):
+        return self.moonList
+    
     def getVolume (self):
         v = 4.0/3 * math.pi * self.radius**3
         return v
@@ -31,3 +42,17 @@ class Planet:
     def getDensity (self):
         d = self.mass / self.getVolume ( )
         return d
+    
+    def getCircumference (self):
+        c = 2 * (math.pi * self.radius)
+
+    #mutator Methods
+
+    def setName (self, newName):
+        self.name = newName
+
+    def setMoons (self, newMoons):
+        self.numMoons += newMoons
+
+    def addMoon (self, newMoonName):
+        self.moonList += newMoonName
